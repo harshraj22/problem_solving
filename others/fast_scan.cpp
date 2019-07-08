b3c1453a2d637e01205f63e22918490f46707983
harshraj22 <harshraj22aug@gmail.com>
2019-07-08 12:43:01 +0530

    added fast scan template

---
+++ others/fast_scan.cpp
@@ -0,0 +1,10 @@
+#define gc getchar_unlocked
+void scanint(int &x){
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}

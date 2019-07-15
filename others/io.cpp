91036c89454bebac6be336a747a7dad44e4f1d32
harshraj22 <harshraj22aug@gmail.com>
2019-07-16 02:20:45 +0530

    added io.cpp

---
+++ others/io.cpp
@@ -0,0 +1,9 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false);	cin.tie(0);	cout.tie(0);
+

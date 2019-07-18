eda550eb19fc3354b2066f0e1912e31f88b10718
harshraj22 <harshraj22aug@gmail.com>
2019-07-18 13:19:13 +0530

    modified io.cpp

--- others/io.cpp
+++ others/io.cpp
@@ -1,9 +1,16 @@
+#pragma warning(disable:4996)
+#pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("-ffloat-store")
 
 #include<bits/stdc++.h>
 using namespace std;
 
 #define IOS ios_base::sync_with_stdio(false);	cin.tie(0);	cout.tie(0);
 
+#define ll long long int 
+
+const int inf = 0x3f3f3f3f;
+const ll INF = 0xFFFFFFFFFFFFFFFL;

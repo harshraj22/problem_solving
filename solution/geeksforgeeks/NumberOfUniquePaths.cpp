3b34d68c151cc012f6ac48f3967ba852cfd3373d
harshraj22 <harshraj22aug@gmail.com>
2020-03-22 17:18:50 +0530

    added python implementation in comments

--- solution/geeksforgeeks/NumberOfUniquePaths.cpp
+++ solution/geeksforgeeks/NumberOfUniquePaths.cpp
@@ -65,3 +65,18 @@ int main(){
 		solve();
 	return 0;
 }
+
+/*
+test = int(input())
+for _ in range(test):
+    n, m = map(int, input().split())
+    dp = [[0 for x in range(m+1)] for y in range(n+1)]
+    dp[n-1][m-1] = 1
+    
+    for i in range(n-1,-1,-1):
+        for j in range(m-1,-1,-1):
+            dp[i][j] += dp[i+1][j] + dp[i][j+1]
+            
+    print(dp[0][0])
+
+*/

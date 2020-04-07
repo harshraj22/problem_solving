5814586b9f7b0935c4aedb7e9a15e428900b4099
harshraj22 <harshraj22aug@gmail.com>
2020-04-07 15:23:17 +0530

    added solution to leetcode type: hard code: intern

---
+++ solution/leetcode/1402.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reducing-dishes/
+
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int ans = 0, n = satisfaction.size();
+        sort(satisfaction.begin(), satisfaction.end());
+    
+        for (int start = 0; start < n; start += 1) {
+            int sum = 0;
+            for (int i = start; i < n; i += 1) {
+                sum += satisfaction[i] * (i-start+1);
+            }
+            ans = max(ans, sum);
+        }
+        
+        return ans;
+    }
+};

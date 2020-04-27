e4bd6cfbfb45b7e74de71ec1bce5c83b34dbed0d
harshraj22 <harshraj22aug@gmail.com>
2020-04-27 16:26:01 +0530

    added topological sort based solution code: intern

---
+++ solution/leetcode/207.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/course-schedule/
+// Simple `Topological Sort` based solution
+// 		if a directed cycle exists, the nodes of cycle will never be visited
+
+class Solution {
+    int n;
+    bool ans = true;
+    vector<bool> vis;
+    vector<int> indegree;
+    vector<vector<int>> graph;
+    
+    void dfs(int node) {
+        if (vis[node]){
+            ans = false;
+            return;
+        } 
+        vis[node] = true;
+        
+        for (auto child: graph[node]) {
+            indegree[child] -= 1;
+            if (indegree[child] == 0)
+                dfs(child);
+        }
+    }
+    
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        n = numCourses;
+        indegree.resize(n); vis.resize(n);
+        graph.resize(n);
+        fill(vis.begin(), vis.end(), false);
+        fill(indegree.begin(), indegree.end(), 0);
+        
+        for (auto it: prerequisites) {
+            int u = it[0], v = it[1];
+            graph[u].push_back(v);
+            indegree[v] += 1;
+        }
+        
+        for (int i = 0; i < n; i += 1) {
+            if (indegree[i] == 0 && vis[i] == false) {
+                dfs(i);
+                indegree[i] -= 1;
+            }
+        }
+        
+        if (accumulate(vis.begin(), vis.end(), 0) != n)
+            ans = false;
+        
+        return ans;
+    }
+};

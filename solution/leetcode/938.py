f182e026a4f4c325852cec69d65c912a10bdd27c
harshraj22 <harshraj22aug@gmail.com>
2020-05-04 18:30:04 +0530

    added tree traversal based solution code: intern

---
+++ solution/leetcode/938.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/range-sum-of-bst/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if root == None:
+            return 0
+        elif L <= root.val <= R:
+            return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        else:
+            return self.rangeSumBST(root.left, L, R)

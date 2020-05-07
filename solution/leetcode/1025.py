11fb8286de0a6b156385f342961206df7f19c944
harshraj22 <harshraj22aug@gmail.com>
2020-05-08 03:13:10 +0530

    added another simple dp based solution code: intern

---
+++ solution/leetcode/1025.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/divisor-game/
+
+class Solution:
+    from math import sqrt
+    def __init__(self):
+        self.cache = dict([(1, False), (2, True), (3, False)])
+        
+    def divisorGame(self, n: int) -> bool:
+        if n in self.cache:
+            return self.cache[n]
+        for i in range(1, int(sqrt(n))+2):
+            if n%i == 0 and not self.divisorGame(n-i):
+                return self.cache.setdefault(n, True)
+        return self.cache.setdefault(n, False)
+        
+'''
+class Solution {
+    map<int, int> cache;
+public:
+    Solution() {
+        cache = {{1, false}, {2, true}, {3, false}};
+    };
+    
+    bool divisorGame(int n) {
+        if (cache.find(n) != cache.end())
+            return cache[n];
+        for (int i = 1; i*i <= n; i += 1)
+            if (n % i == 0 && divisorGame(n-i) == false)
+                return cache[n] = true;
+        return cache[n] = false;
+    }
+};
+'''

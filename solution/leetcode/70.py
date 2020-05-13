19d29369586e4c3f30ccbc8048261986abaffdee
harshraj22 <harshraj22aug@gmail.com>
2020-05-14 01:09:56 +0530

    added another simple dp based solution code: intern

---
+++ solution/leetcode/70.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/climbing-stairs/
+# Fibonachi based easy DP
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0 for _ in range(n+1)]
+        dp[1:3] = [1, 1]
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]

2cf7a7e197afba45ac8032746ae1b6f973d93987
harshraj22 <harshraj22aug@gmail.com>
2020-05-14 00:49:00 +0530

    added another simple dp problem code: intern

---
+++ solution/leetcode/121.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    from math import inf 
+    
+    def maxProfit(self, prices: List[int]) -> int:
+        n, min_so_far, max_profit = len(prices), inf, 0
+        dp = [[0 for _ in prices] for __ in [1, 2]]
+        
+        # dp[0], dp[1] = buy, sell
+        for index, price in enumerate(prices):
+            dp[0][index], dp[1][index] = -price, price - min_so_far
+            min_so_far = min(min_so_far, price)
+            max_profit = max(max_profit, dp[1][index])
+            
+        return max_profit

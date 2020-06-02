0a1e2d35e052ea4d44c9ab13636630ef07f81011
harshraj22 <harshraj22aug@gmail.com>
2020-06-03 01:55:22 +0530

    added tree traversal based solution code: intern

---
+++ solution/leetcode/1448.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int goodNodes(TreeNode* root, int mx = -100000) {
+        if (root == nullptr)
+            return 0;
+        return (root->val >= mx) + goodNodes(root->left, max(mx, root->val)) + goodNodes(root->right, max(mx, root->val));
+    }
+};

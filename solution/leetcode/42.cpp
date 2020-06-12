45cc811e8645a2f321507c57abd7526da7b71d8a
harshraj22 <harshraj22aug@gmail.com>
2020-06-12 22:15:46 +0530

    added solution based on simple logic code: intern

---
+++ solution/leetcode/42.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size(), water = 0;
+        vector<int> pref(n), suff(n);
+
+        // pref[i]: nearest maxima (index < i)
+        for (int i = 1; i < n; i += 1) 
+            pref[i] = max(pref[i-1], height[i-1]);
+
+        // suff[i]: nearest maxima (index > i)
+        for (int i = n-2; i >= 0; i -= 1)
+            suff[i] = max(suff[i+1], height[i+1]);
+
+        for (int i = 1; i+1 < n; i += 1) {
+            int h = min(pref[i], suff[i]);
+            water += max(0, h-height[i]);
+        }
+
+        return water;
+    }
+};

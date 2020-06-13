07609470f912ca3f485c0801790bdcd40a9236e2
harshraj22 <harshraj22aug@gmail.com>
2020-06-13 21:22:17 +0530

    added good solution using recursion for finding lca on tree code: intern

---
+++ solution/leetcode/236.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// Single Pass, Time: O(n), Space: O(n) recursion stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return nullptr;
+        else if (root == p || root == q)
+            return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left != nullptr && right != nullptr)
+            return root;
+        else if (left != nullptr) 
+            return left;
+        return right;
+    }
+};

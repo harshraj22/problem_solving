18c60876eec060e3a564a8ebbb949f35d4e08bee
harshraj22 <harshraj22aug@gmail.com>
2020-06-16 18:04:18 +0530

    modified solution

--- solution/leetcode/22.py
+++ solution/leetcode/22.py
@@ -23,6 +23,17 @@ class Solution:
         return solve_for(n)
         
 
+class Solution:
+    def generateParenthesis(self, n: int, open=0, closed=0, s='') -> List[str]:
+        if closed > open:
+            return []
+        elif closed == n:
+            return [s]
+        elif open == n:
+            return [s + ')'*(n-closed)]
+        return self.generateParenthesis(n, open+1, closed, s+'(') + self.generateParenthesis(n, open, closed+1, s+')')
+
+
 '''
 # My original solution
 class Solution:

f9b8a1f3534a37e8833f6e8447a47acb5e76171a
harshraj22 <harshraj22aug@gmail.com>
2020-07-02 20:18:31 +0530

    added another simple dp based solution code: intern

--- solution/geeksforgeeks/ShortestCommonSupersequence.cpp
+++ solution/geeksforgeeks/ShortestCommonSupersequence.cpp
@@ -1,47 +1,37 @@
 // https://practice.geeksforgeeks.org/problems/shortest-common-supersequence/0
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
-
-int dp[100][100], n, m;
 string s1, s2;
+vector<vector<int>> dp;
 
-int recur(int l, int r) {
-	if (l == n) return m - r;
-	else if (r == m) return n - l;
-	else if (dp[l][r] != -1)
-		return dp[l][r];
-	
-	dp[l][r] = 1 + min(recur(l+1, r), recur(l, r+1));
-
-	if (s1[l] == s2[r])
-		dp[l][r] = min(dp[l][r], 1 + recur(l+1, r+1));
-
-	return dp[l][r];
+int shortestCommonSubseq(int n, int m) {
+	if (n == s1.size())
+		return abs(m - (int)s2.size());
+	else if (m == s2.size())
+		return abs(n - (int)s1.size());
+	else if (dp[n][m] != -1)
+		return dp[n][m];
+	else if (s1[n] == s2[m])
+		return dp[n][m] = 1 + shortestCommonSubseq(n+1, m+1);
+	return dp[n][m] = 1 + min(shortestCommonSubseq(n+1, m), shortestCommonSubseq(n, m+1));
 }
 
 void solve() {
-	for (int i = 0; i < 100; i += 1) {
-		for (int j = 0; j < 100; j += 1)
-			dp[i][j] = -1;
-	}
-
 	cin >> s1 >> s2;
-	n = s1.size(); m = s2.size();
-
-	cout << recur(0, 0) << '\n';
+	int n = s1.size(), m = s2.size();
+	dp = vector<vector<int>> (n, vector<int> (m, -1));
+	cout << shortestCommonSubseq(0, 0) << '\n';
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    
-    int test = 1;
-    cin >> test;
-    while(test--)
-        solve();
-	
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);	cout.tie(0);
+
+	int test;	cin >> test;
+	while (test--)
+		solve();
+
 	return 0;
 }

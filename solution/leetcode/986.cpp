15879cd3e72ce7ccfdcbe302ca31bf0d6372e27a
harshraj22 <harshraj22aug@gmail.com>
2021-02-19 15:07:03 +0530

    added two pointer based solution code: place

--- solution/leetcode/986.cpp
+++ solution/leetcode/986.cpp
@@ -37,3 +37,41 @@ public:
         return ans;
     }
 };
+
+
+/*
+Two pointer based solution
+class Solution {
+    bool intersect(vector<int> a, vector<int> b) {
+        if (a[0] > b[1] || a[1] < b[0])
+            return false;
+        return true;
+    }
+
+    vector<int> common(vector<int> a, vector<int> b) {
+        return { max(a[0], b[0]), min(a[1], b[1]) };
+    }
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> ans;
+
+        int i = 0, n = secondList.size();
+        if (n == 0) return ans;
+        
+        for (auto first: firstList) {
+            while (i < n && !intersect(first, secondList[i]) && first > secondList[i])
+                i += 1;
+            if (i < n && !intersect(first, secondList[i]))
+                continue;
+            while (i < n && intersect(first, secondList[i])) {
+                ans.push_back(common(first, secondList[i])); 
+                i += 1;
+            }
+            
+            i -= 1;
+        }
+
+        return ans;
+    }
+};
+*/

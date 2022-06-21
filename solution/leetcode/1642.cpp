0521fec7ebb66c984bac1619423a59a3fda35b62
Harsh Raj <harshraj22aug@gmail.com>
2022-06-21 17:31:38 +0530

    Good greedy solution
    Solved again using Python

--- solution/leetcode/1642.cpp
+++ solution/leetcode/1642.cpp
@@ -38,3 +38,56 @@ public:
         return index - 1;
     }
 };
+
+/*
+
+class Solution:
+    from collections import defaultdict
+    
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        from sortedcontainers import SortedList
+        
+        used = defaultdict(SortedList)
+        # maintain a sorted ordered list of differences in heights
+        # fill the smaller ones with brick, and larger ones with ladders
+        
+        # once a new difference of height arrives, try to fill it with
+        # brick, it might require converting the larger difference (which is
+        # currently filled with brick) to ladder
+        for index, height in enumerate(heights[1:], 1):
+            diff = height - heights[index-1] 
+            if diff <= 0:
+                continue
+            elif diff <= bricks:
+                used['bricks'].add(diff)
+                bricks -= diff
+                continue
+            elif ladders == 0:
+                return index - 1
+            
+            while bricks < diff:
+                if ladders == 0:
+                    return index-1
+                
+                if len(used['bricks']) == 0 or used['bricks'][-1] < diff:
+                    used['ladders'].add(diff)
+                    ladders -= 1
+                    break
+                
+                brick = used['bricks'][-1]
+                used['bricks'].pop()
+                
+                ladders -= 1
+                used['ladders'].add(brick)
+                bricks += brick
+                
+            if bricks >= diff:
+                used['bricks'].add(diff)
+                bricks -= diff
+                
+                
+        return len(heights) - 1
+            
+                    
+
+*/

81a7a033a56f5e8b51158aa5ebe895823ead92a7
Harsh Raj <harshraj22aug@gmail.com>
2022-07-07 22:40:06 +0530

    Added dp based sol

--- solution/leetcode/97.cpp
+++ solution/leetcode/97.cpp
@@ -57,3 +57,25 @@ public:
         return recur(0, 0, 0, s1, s2, s3);
     }
 };
+
+
+/*
+class Solution:
+    from functools import lru_cache
+    
+    @lru_cache
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if not s1:
+            return s2 == s3
+        elif not s2:
+            return s1 == s3
+        elif not s3:
+            return False
+        if s3[0] not in [s1[0], s2[0]]:
+            return False
+        elif s3[0] != s2[0]:
+            return self.isInterleave(s1[1:], s2, s3[1:])
+        elif s3[0] != s1[0]:
+            return self.isInterleave(s1, s2[1:], s3[1:])
+        else: return self.isInterleave(s1[1:], s2, s3[1:]) or self.isInterleave(s1, s2[1:], s3[1:])
+*/

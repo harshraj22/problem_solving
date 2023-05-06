a233f8ac05af515de95587840dc819b2056fd66e
Harsh Raj <harshraj22aug@gmail.com>
2023-05-06 16:43:37 +0530

    solved again

--- solution/leetcode/104.py
+++ solution/leetcode/104.py
@@ -11,7 +11,7 @@ class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if root == None:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) 
 
 '''
 # Definition for a binary tree node.

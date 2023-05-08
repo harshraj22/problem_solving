299b547b5075c93c3f1ffc0dd31438a4bf50f31e
Harsh Raj <harshraj22aug@gmail.com>
2023-05-08 22:37:49 +0530

    simple select in sql

---
+++ solution/leetcode/595.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/big-countries/description/
+
+# Write your MySQL query statement below
+select name, population, area from world where area >= 3000000 or population >= 25000000;
